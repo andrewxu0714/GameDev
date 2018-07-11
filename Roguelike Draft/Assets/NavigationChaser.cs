7c9fd5a3b492ce710f9ee1dc255f2a275136dbd7
Andrew Xu <andrewxu0714@gmail.com>
2018-07-12 01:07:10 +1000

    Improved navigation, removed the use of navagents and now just using path directly

--- Roguelike Draft/Assets/NavigationChaser.cs
+++ Roguelike Draft/Assets/NavigationChaser.cs
@@ -1,27 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavigationChaser : MonoBehaviour {
 
-	public GameObject navLead;
 	public Vector3 goal;
 	private Body bodyScript;
+	private NavMeshPath movePath;
+	private int trackPath;
 
 	// Use this for initialization
 	void Start () {
+		movePath = new NavMeshPath ();
 		bodyScript = GetComponent<Body> ();
-		navLead = Instantiate (navLead, this.transform);
-		navLead.GetComponent<NavigationLead> ().chaser = this.gameObject;
+		goal = new Vector3(29,0.5f,29);
+		trackPath = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//goal = this.transform.position + Vector3.right;
-		goal = new Vector3(32,0,32);
-		navLead.GetComponent<NavigationLead> ().goal = goal;
-		if (bodyScript.CanAct ()) {
-			bodyScript.translate = Mathf.Min(10f*Time.deltaTime,(navLead.transform.position - this.transform.position).magnitude)*(navLead.transform.position - this.transform.position).normalized;
+		NavMesh.CalculatePath (transform.position, goal, NavMesh.AllAreas, movePath);
+		if (bodyScript.CanAct () && movePath.corners.Length>1) {
+			Vector3 positionAdjust = new Vector3 (this.transform.position.x, 0, this.transform.position.z);
+			Vector3 targetAdjust = new Vector3 (movePath.corners [1].x, 0, movePath.corners [1].z);
+			Debug.Log ((targetAdjust - positionAdjust).magnitude);
+			if ((targetAdjust - positionAdjust).magnitude < (10f * Time.deltaTime)) {
+				bodyScript.translate = targetAdjust - positionAdjust;
+			} else {
+				bodyScript.translate = (targetAdjust - positionAdjust).normalized * 10f * Time.deltaTime;
+			}
 		}
+		for (int i = 0; i < movePath.corners.Length-1; i++)
+			Debug.DrawLine(movePath.corners[i], movePath.corners[i+1], Color.red);	
 	}
 }
